5b48e279b6c09737669499e4b35c457bb053df44
Michella <michella.reaidy@gmail.com>
2025-04-08 21:41:31 +0300

    db changing with postgresql

--- config/db.js
+++ config/db.js
@@ -5,21 +5,17 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 // Create a new Sequelize instance with database credentials from environment variables
-
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
-    dialectModule: require('mysql2'),
+    port: process.env.DB_PORT || 5432,  // Default port for PostgreSQL is 5432
+    dialect: process.env.DB_DIALECT || 'postgres',  // Use 'postgres' for PostgreSQL
     logging: console.log
 });
- 
-// Synchronize the database schema without forcing a drop (force: false means it won't delete existing tables)
 
+// Synchronize the database schema without forcing a drop (force: false means it won't delete existing tables)
 sequelize.sync({ force: false })
     .then(() => console.log('Tables are checked'))
     .catch(err => console.error('Sequelize Sync Error:', err));
 
-    // Export the sequelize instance for use in other parts of the project
-
+// Export the sequelize instance for use in other parts of the project
 module.exports = sequelize;
